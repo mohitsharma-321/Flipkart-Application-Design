bc6ec5e90772ff4158f7ca2594d18005ddb72838
Mohit Sharma <chiragmohitsharma@gmail.com>
2026-03-03 01:18:17 +0530

    Added Sort Strategies Logic

--- src/main/java/com/flipkartdaily/strategy/sort/PriceSortStrategy.java
+++ src/main/java/com/flipkartdaily/strategy/sort/PriceSortStrategy.java
@@ -5,8 +5,19 @@ import com.flipkartdaily.model.Item;
 import java.util.Comparator;
 
 public class PriceSortStrategy implements SortStrategy{
+
+    private boolean asc;
+
+    public PriceSortStrategy(boolean asc) {
+        this.asc = asc;
+    }
     @Override
     public Comparator<Item> getComparator() {
-        return null;
+        Comparator<Item> comparator = Comparator.comparing(Item::getPrice);
+
+        if(!asc) {
+            comparator = comparator.reversed();
+        }
+        return comparator;
     }
 }
--- src/main/java/com/flipkartdaily/strategy/sort/QuantitySortStrategy.java
+++ src/main/java/com/flipkartdaily/strategy/sort/QuantitySortStrategy.java
@@ -5,8 +5,19 @@ import com.flipkartdaily.model.Item;
 import java.util.Comparator;
 
 public class QuantitySortStrategy implements SortStrategy{
+
+    private boolean asc;
+
+    public QuantitySortStrategy(boolean asc) {
+        this.asc = asc;
+    }
     @Override
     public Comparator<Item> getComparator() {
-        return null;
+        Comparator<Item> comparator = Comparator.comparing(Item::getPrice);
+
+        if(!asc) {
+            comparator = comparator.reversed();
+        }
+        return comparator;
     }
 }
